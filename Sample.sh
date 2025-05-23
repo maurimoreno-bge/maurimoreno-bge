cf3cd285f377432a78a533804aaee302291f35c0
mauri Moreno <mauri.moreno.vargas@gmail.com>
2025-05-23 17:53:42 -0400

    Add files via upload

---
+++ Sample.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo " ____  ____  ____ "
+echo " |___| | __  |__ "
+echo " |___| |___| |___ "
+# Nombre : mauri Moreno
+# Fecha : 22/may/2025
+echo -e "\e[0;32mDate\e[0m"
+date
+echo -e "\e[0;32mEnter Password :\e[0m"
+read xx 
+if [ " $xx " =  " 3456 " ];
+then  
+echo " x "
+echo -e "\e[1;34mHillel\e[0m"
+ping -c 11 127.0.0.1
+echo -e "\e[0;32mCO_de\e[0m"   
+id                 
+elif [ " $xx " = " Truly " ];
+then
+echo " true "       
+else              
+echo " false "                   
+fi
+exit
